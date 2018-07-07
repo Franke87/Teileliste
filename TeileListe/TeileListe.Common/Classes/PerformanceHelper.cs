0ddba44ba3578e69b63ef9c30b13690e9ff8bdac
Christian Simon <christian.simon.1987@outlook.com>
2018-07-07 17:04:07 +0200

    Performancemessung eingebaut für den Sichernbutton und den Zurücksetzenbutton.

---
+++ TeileListe/TeileListe.Common/Classes/PerformanceHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace TeileListe.Common.Classes
+{
+    public static class PerformanceHelper
+    {
+        public static void Messung(Action action, string beschreibung, int wiederholungen)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            action();
+
+            stopwatch.Stop();
+
+            File.AppendAllText("log.txt",
+                                string.Format("{0} s and {1} ms für {2} Wiederholungen von {3}{4}",
+                                stopwatch.Elapsed.Seconds,
+                                stopwatch.Elapsed.Milliseconds,
+                                wiederholungen,
+                                beschreibung,
+                                Environment.NewLine));
+        }
+    }
+}
