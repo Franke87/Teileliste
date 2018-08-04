7dacd592fb292717f917d8741249b9e7e0607e38
Christian Simon <christian.simon.1987@outlook.com>
2018-08-04 15:42:10 +0200

    Kein Scrollbalken bei der Fahrradauswahl.

--- TeileListe/TeileListe/Teileliste/UserControl/FahrradAuswahlControl.xaml.cs
+++ TeileListe/TeileListe/Teileliste/UserControl/FahrradAuswahlControl.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace TeileListe.Teileliste.UserControl
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TeileListe.Teileliste.UserControl
 {
     /// <summary>
     /// Interaction logic for FahrradAuswahlControl.xaml
@@ -8,6 +12,16 @@
         public FahrradAuswahlControl()
         {
             InitializeComponent();
+        }
+
+        private void CheckForToolTipNeeded(object sender, ToolTipEventArgs e)
+        {
+            var test = sender as TextBlock;
+            if (test != null && test.TextTrimming != TextTrimming.None)
+            {
+                test.Measure(new Size(Double.PositiveInfinity, Double.PositiveInfinity));
+                e.Handled = test.ActualWidth >= test.DesiredSize.Width;
+            }
         }
     }
 }
